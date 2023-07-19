579f4abb8ede5a1978c4b813d93a5f01503e20e5
Abhiraj Rik <BHK8624@dpsjaipur.com>
2023-07-19 20:16:42 +0530

    add parser
    now config parser works :)

--- src/main.py
+++ src/main.py
@@ -1,32 +1,51 @@
 '''
 the source code for VirboxQuest
 '''
-
 from json import loads, dumps
 from os import getlogin, mkdir
 from os.path import isfile
+import sys
 
 cpath = f"/home/{getlogin()}/.config/VirboxQuest/data.json"
+config = {"boxcoins": 0, "levels_completed": 0}
 
 def check_config():
+    '''
+    This function checks config file
+    '''
     if isfile(cpath) == True:
-        return 0
+        return True
     else:
         try:
             mkdir(f"/home/{getlogin()}/.config/VirboxQuest/")
         except FileExistsError:
-            with open(cpath, 'w', encoding="utf-8") as config:
-                config.write('')
-                
-            config.close()
-            return 0
-
-        with open(cpath, 'w', encoding="utf-8") as config:
-            config.write()
-                
-        config.close()
-        return 0
+            config_file = open(cpath, 'w')
+            config_file.write(str(dumps(config)))
+            config_file.close()
+            return True
         
+        config_file = open(cpath, 'w')
+        config_file.write(str(dumps(config)))
+        config_file.close()
+        return True
 
+def parse_config():
+    global config
+    '''
+    This function parses config file
+    '''
+    with open(cpath, 'r', encoding="utf-8") as config_file:
+        config_data = loads(config_file.read())
+    config_file.close()
+
+    if "boxcoins" and "levels_completed" in list(config_data.keys()):
+        config["boxcoins"] = config_data["boxcoins"]
+        config["levels_completed"] = config_data["levels_completed"]
+    else:
+        print("\x1b[31m!\x1b[0m config error..")
+        sys.exit(1)
+        
+    
 if __name__ == "__main__":
     check_config()
+    parse_config()
