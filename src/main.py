a13640281b6f9db41ed39795d3af10e8d61e28ed
Abhiraj Rik <BHK8624@dpsjaipur.com>
2023-07-19 17:49:27 +0530

    add config file

--- src/main.py
+++ src/main.py
@@ -1,26 +1,32 @@
-"""
-MIT License
+'''
+the source code for VirboxQuest
+'''
 
-Copyright (c) 2023 Abhiraj Rik
+from json import loads, dumps
+from os import getlogin, mkdir
+from os.path import isfile
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+cpath = f"/home/{getlogin()}/.config/VirboxQuest/data.json"
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+def check_config():
+    if isfile(cpath) == True:
+        return 0
+    else:
+        try:
+            mkdir(f"/home/{getlogin()}/.config/VirboxQuest/")
+        except FileExistsError:
+            with open(cpath, 'w', encoding="utf-8") as config:
+                config.write('')
+                
+            config.close()
+            return 0
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+        with open(cpath, 'w', encoding="utf-8") as config:
+            config.write()
+                
+        config.close()
+        return 0
+        
 
-"""
-
-print("this works!")
+if __name__ == "__main__":
+    check_config()
