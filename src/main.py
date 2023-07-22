478d0ed595e450db2b38b33fce51d91b225bce26
Abhiraj Rik <BHK8624@dpsjaipur.com>
2023-07-22 18:38:39 +0530

    level 1 partil

--- src/main.py
+++ src/main.py
@@ -5,10 +5,13 @@ from json import loads, dumps
 from os import getlogin, mkdir
 from os.path import isfile
 import sys
+from random import randint
 
 CPATH = f"/home/{getlogin()}/.config/VirboxQuest/data.json"
 CONFIG = {"boxcoins": 0, "levels_completed": 0, "items_owned": []}
 STORE = {"VIM_MANUAL": ["Vim Cheatsheet", "A simple cheatsheet for vim", 10], "ANTI_GRASS": ["Grass Avoider", "A tool to help from avoiding grass", 100]} 
+DEF_POS = int(0)
+USER_POS = 0
 
 def check_config():
     '''
@@ -104,21 +107,77 @@ def store():
             print("\x1b[31m!\x1b[0m invalid choice")
         
 def startup():
-    print("Welcome to VirboxQuest...\nin this game of adventure you will be travelling through the world of Editoria... where people fight for editors...\n")
-    print("In this game you will face adventures to save a vim user \"Virbox\" is kidnapped by some VSC newbie.. you need to save him...\n")
+    print("Since Microssia invaded Gkaine... Many vim users are sent into the grasslands of Micsow..")
+    print("Welcome fellow traveller.. you are currently in Editoria and now you have to go through a series of adventures to find Virbox...")
 
 def level_1():
-    print("\x1b[36m!\x1b[0m knowing the basics of vim... go to the house of \"Vim Diesel\"")
+    global USER_POS
+    print("\x1b[36m!\x1b[0m you are in the neighbourhood of Viev.. now go through 4 houses and find the vim guy")
+    l1_map = '''\x1b[34m!\x1b[0m map for this level:
+    |1|
+    |2|
+    |3|
+    |4|
+    |5|
+    |6|
+    |7|
+    |8|
+    |9|
+  --10|
+  | 11| 
+  --12|
+   |13|
+   |14|
+   |15--|
+   |16  |
+   |17--|
+  --18|
+  | 19|
+  --20--
+   |21 |
+  |22 --
+    '''
+    VIM_USER = randint(1, 4)
+    H1 = 11
+    H2 = 16
+    H3 = 19
+    H4 = 22
+    VISIT_H1 = False
+    VISIT_H2 = False
+    VISIT_H3 = False
+    VISIT_H4 = False
+    VIM_UH = None
+    
+    if VIM_USER == 1:
+        VIM_UH = 11
+    elif VIM_USER == 2:
+        VIM_UH = 16
+    elif VIM_USER == 3:
+        VIM_UH = 19
+    else:
+        VIM_UH = 22
+
     while True:
         choice = input("\x1b[32m>\x1b[0m command: ")
         if choice == "help":
-            print("\x1b[34mmap\x1b[0m: view map\n\x1b[34mleft\x1b[0m: move left\n\x1b[34mback\x1b[0m: move back\n\x1b[34mfront\x1b[0m: move forward\n\x1b[34mhelp\x1b[0m: show this menu\n\x1b[34mchest\x1b[0m: view stuff\n\x1b[34mstore\x1b[0m: buy items")
+            print("\x1b[34mmap\x1b[0m: view map\n\x1b[34mback\x1b[0m: move back\n\x1b[34mfront\x1b[0m: move forward\n\x1b[34mhelp\x1b[0m: show this menu\n\x1b[34mchest\x1b[0m: view stuff\n\x1b[34mstore\x1b[0m: buy items")
         elif choice == "store":
             store()
         elif choice == "exit":
             exit_game()
         elif choice == "chest":
             chest()
+        elif choice == "front":
+            if USER_POS >= 25:
+                print("\x1b[33m!\x1b[0m end of world")
+            elif USER_POS == 11 and VIM_UH == 11 and VISIT_H1 == False or USER_POS == 16 and VIM_UH == 16 and VISIT_H2 == False or USER_POS == 19 and VIM_UH == 19 and VISIT_H3 == False or USER_POS == 22 and VIM_UH == 22 and VISIT_H4 == False:
+                print("\x1b[32m!\x1b[0m you found the vim user... + 10 boxcoins")
+                print("\x1b[32m!\x1b[0m congrats level 1 passed")
+                break
+            else:
+                USER_POS = USER_POS+1
+        elif choice == "map":
+            print(l1_map)
         else:
             print("\x1b[31m!\x1b[0m invalid choice")
 
